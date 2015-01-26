7488ad2654ae1aefd80f7ab0fc1fb8a02ead8c2b
Warrick Gilbert <warrick_gilbert@yahoo.co.nz>
2015-01-26 15:42:56 -0800

    finished Jukebox-2 assignment

--- jukebox.js
+++ jukebox.js
@@ -1,14 +1,29 @@
-// promptAndPlay
-//
-// Prompts the user to enter a song.
-// Plays the entered song.
-// Repeats when the song is complete.
-//
-var promptAndPlay = function() {
-  var songString = prompt("Welcome to JukeBox!\nEnter a song to play:");
-  var song = parseSong(songString);
-  playSong(song, 500, promptAndPlay);
-};
+$(document).ready(function() {
+  // promptAndPlay
+  //
+  // Prompts the user to enter a song.
+  // Plays the entered song.
+  // Repeats when the song is complete.
+  //
 
-// Get things going.
-promptAndPlay();
+
+  var promptAndPlay = function() {
+    var songString = prompt("Welcome to JukeBox!\nEnter a song to play:", "F#*1 F#*1 D*1 B*1 B*1 E*1 E*1 E*1 G#*1 G#*1 A*1 B*1");
+    $("#play-button").html("Playing...");    
+    var song = parseSong(songString);
+    playSong(song, 400, function() {
+        $("#play-button").removeAttr("disabled");
+        $("#play-button").html("Play");
+    });
+  };
+
+  // Get things going.
+  // promptAndPlay();
+
+  $("#play-button").click(function(){
+    promptAndPlay();
+    $("#play-button").attr("disabled", "disabled")
+  });
+
+
+});
